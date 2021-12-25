cca2fdc13006065a2eed824904fdff714a438843
Hardy173 <izvesten73@mail.ru>
2021-12-25 15:01:46 +0400

    Added prettier and eslint

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Props = {}
+
+const Main: FC<Props> = () => {
+  return <div>Main</div>
+}
+
+export default Main
